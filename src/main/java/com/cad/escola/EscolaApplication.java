d90232a98fc0ee54626dcea7631ceaf6b279f9d5
Rodrigo Cristian <cristianpedrorodrigo@gmail.com>
2023-07-16 20:23:01 -0300

    Criado classe curso

--- src/main/java/com/cad/escola/EscolaApplication.java
+++ src/main/java/com/cad/escola/EscolaApplication.java
@@ -8,6 +8,12 @@ public class EscolaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscolaApplication.class, args);
+		Curso curso = new Curso("AM", "Analista de dado");
+		
+		curso.setValor(1.70);
+		
+		System.out.println ();
+		System.out.println ();
 	}
 
 }
